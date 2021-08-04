3b135e6d94ae4bff079befa70d8abd762a67d85f
Nicola Yasol <nicolay324@gmail.com>
2021-08-04 15:22:25 -0400

    Search icon opens search page

---
+++ script.js
@@ -0,0 +1,6 @@
+/*On Click event for search icon to open search page*/
+document.getElementById("searchBtn").onclick = function() {myFunction()};
+
+function myFuction() {
+    location.href = "search.html";
+}
